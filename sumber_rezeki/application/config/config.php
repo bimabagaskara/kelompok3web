0dff7831a5dc35d55458ee8219ca9f4082070cd1
bimabagaskara <55170641+bimabagaskara@users.noreply.github.com>
2020-06-29 19:17:25 +0700

    rest API

--- sumber_rezeki/application/config/config.php
+++ sumber_rezeki/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sumber_rezeki/';
+$config['base_url'] = 'http://localhost/kelompok3web/sumber_rezeki/';
 
 /*
 |--------------------------------------------------------------------------
